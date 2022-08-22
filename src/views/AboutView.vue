79a6908420320ed28d5cc6ad2dc58f6ce9a3e45f
Genesis <genesis.dmop@gmail.com>
2022-08-22 16:08:20 -0500

    login

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+   <v-container>
+                <v-text-field 
+                  text="Nombre de usuario o correo electrónico"
+                  filled
+              ></v-text-field>
+              <v-text-field
+                  placeholder="Contraseña"
+                  filled
+              ></v-text-field>
+           </v-container>
+  </div>
+</template>
