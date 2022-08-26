c0812ff0743d0ff287e14e22f8e25d76442917ef
Josué David <josdavsm88@gmail.com>
2022-08-26 15:56:15 -0500

    SideBar

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,21 @@
 <template>
-  <div id="app">
-    <nav>
-      <v-layout> 
-        <img src="./assets/logo.png" alt="" height="35px">
-        <v-flex xs12>
-          <router-link to="/">Inicio</router-link> |
-          <router-link to="/about">Acceso</router-link>
-        </v-flex>
-      </v-layout>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <SideBar />
+    <v-main>
+      <router-view>
+        
+      </router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  background-color: #B2D4CC;
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #3097C3;
-    }
-  }
-}
-</style>
+<script>
+import SideBar from "./components/SideBar.vue";
+export default {
+  name: 'App',
+    setup() {
+    },
+    components: { SideBar },
+    data: () => ({}),
+};
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,7 @@
 <template>
-  <div class="about">
-   <v-container>
-                <v-text-field 
-                  text="Nombre de usuario o correo electrónico"
-                  filled
-              ></v-text-field>
-              <v-text-field
-                  placeholder="Contraseña"
-                  filled
-              ></v-text-field>
-           </v-container>
-  </div>
+<h1>hola</h1>
 </template>
+
+<script>
+
+</script>
