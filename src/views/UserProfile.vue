8471dff789c3d846be2cc9b4fc1dd7287cf878b2
Josué David <josdavsm88@gmail.com>
2022-09-01 16:02:26 -0500

    dashboard ventas

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -41,13 +41,18 @@
           :complete="e1 > 2"
           step="2"
         >
-          Name of step 2
+          Datos fiscales
         </v-stepper-step>
   
         <v-divider></v-divider>
   
         <v-stepper-step step="3">
-          Name of step 3
+          Datos Cofepris
+        </v-stepper-step>
+        <v-divider></v-divider>
+  
+        <v-stepper-step step="4">
+          Final
         </v-stepper-step>
       </v-stepper-header>
   
@@ -241,9 +246,7 @@
             Continue
           </v-btn>
   
-          <v-btn text>
-            Cancel
-          </v-btn>
+         
         </v-stepper-content>
   
         <v-stepper-content step="2">
@@ -321,52 +324,8 @@
                     filled
                   />
                 </v-col>
-               <!--  <v-col
-                  cols="12"
-                  md="6"
-                >
-                <p class="text-left">{{ radios || '¿Cuenta con Constancia de Situación Fiscal actualizada?' }}</p>
+                  
                 
-                <v-radio-group
-                    v-model="row"
-                    row
-                  >
-                    <v-radio
-                      label="Si"
-                    
-                      @click="expand = !expand"
-             
-    
-                      
-                    ></v-radio>
-
-                    <v-radio
-                      label="No"
-                      @click="isdeleted"
-                    ></v-radio>
-
-                    <v-radio
-                      label="En trámite"
-              
-                    ></v-radio>
-                  </v-radio-group>
-                </v-col>
-
-                <v-col 
-                 
-                  cols="12"
-                  md="6"
-                >
-
-                <v-file-input 
-                v-show="expand"
-                    label="Subir Constancia de Situación Fiscal actualizada (PDF)"
-                    outlined
-                    dense
-                  ></v-file-input>
-                </v-col> -->
-                
-              
                 <v-card-text>
                   <p class="text-CENTER">{{ radios || '¿Cuenta con Constancia de Situación Fiscal actualizada?' }}</p>
                         <v-row >
@@ -554,38 +513,352 @@
               </v-row>
               
         </v-tab-item>
+
+   <!-- ------------------Persona moral------------------------- -->
         <v-tab-item>
-          <v-card flat>
-              <p>
-                Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Sed hendrerit. Maecenas malesuada. Vestibulum ullamcorper mauris at ligula. Proin faucibus arcu quis ante.
-              </p>
-          </v-card>
+         <v-row>
+          <v-col
+                  cols="12"
+                  md="4"
+                >
+                  <v-text-field
+                    color="purple"
+                    label="Denominación o razón social"
+                    filled
+                  />
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                  <v-text-field
+                    color="purple"
+                    label="RFC Persona moral"
+                    filled
+                  />
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                  <v-text-field
+                    color="purple"
+                    label="Correo electrónico fiscal"
+                    filled
+                  />
+                </v-col>
+                <v-card-text>
+                  <p class="text-CENTER">{{ radios || '¿Cuenta con Constancia de Situación Fiscal actualizada?' }}</p>
+                        <v-row >
+                          <v-col 
+                          cols="12"
+                          md="1"
+                        >
+                            <v-checkbox
+                                label="Si"
+                                  v-model="enabled"
+                                  class="shrink mr-2 mt-0"
+                                  
+                              ></v-checkbox>
+                            </v-col>
+                            <v-col 
+                          cols="12"
+                          md="4"
+                        >
+                          <v-file-input
+                              :disabled="!enabled"
+                              label="Subir documento PDF"
+                              outlined
+                              filled  
+                              dense
+                              
+                    ></v-file-input>
+                            </v-col>
+                       
+                              <v-col 
+                                cols="12"
+                                md="3"
+                              >
+                                <v-checkbox
+                                    v-model="checkbox"
+                                    hide-details
+                                    label="En trámite"
+                                    class="shrink mr-2 mt-0"
+                                  ></v-checkbox>
+                              </v-col>
+
+                              <v-col 
+                                cols="12"
+                                md="3"
+                              >
+                                  <v-checkbox
+                                    v-model="checkbox"
+                                    label="No"
+                                    hide-details
+                                    class="shrink mr-2 mt-0"
+                                  ></v-checkbox>
+                              </v-col>
+                        </v-row>
+                        
+                </v-card-text>
+         </v-row>
         </v-tab-item>
       </v-tabs-items>
-      
+    
           <v-btn
             color="primary"
             @click="e1 = 3"
           >
             Continue
           </v-btn>
-  
+
           <v-btn
             @click="e1 = 1">
             Atras
           </v-btn>
+       
         </v-stepper-content>
   
         <v-stepper-content step="3">
-          <v-card
-            class="mb-12"
-            color="grey lighten-1"
-            height="200px"
-          ></v-card>
-  
+          <v-row>
+            <v-col
+                  cols="12"
+                  md="4"
+                >
+                  <v-text-field
+                    color="purple"
+                    label="Tipo"
+                    filled
+                  />
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                  <v-text-field
+                    color="purple"
+                    label="Nombre del establecimiento"
+                    filled
+                  />
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                  <v-text-field
+                    color="purple"
+                    label="RFC"
+                    filled
+                  />
+                </v-col>
+                
+                <v-card-text>
+                <p class="text-CENTER">{{ radios || '¿Cuenta con logotipo original y propio?' }}</p>
+                        <v-row >
+                          <v-col 
+                          cols="12"
+                          md="1"
+                        >
+                            <v-checkbox
+                                label="Si"
+                                  v-model="enabled"
+                                  class="shrink mr-2 mt-0"
+                                  
+                              ></v-checkbox>
+                            </v-col>
+                            <v-col 
+                          cols="12"
+                          md="6"
+                        >
+                          <v-file-input
+                          
+                              :disabled="!enabled"
+                              label="Subir documento PDF"
+                              outlined
+                              filled  
+                              dense
+                              
+                    ></v-file-input>
+                            </v-col>
+                            <v-col 
+                                cols="12"
+                                md="2"
+                              >
+                                <v-checkbox
+                                    v-model="checkbox"
+                                    hide-details
+                                    label="En proceso "
+                                    class="shrink mr-2 mt-0"
+                                  ></v-checkbox>
+                              </v-col>
+                              <v-col 
+                                cols="12"
+                                md="2"
+                              >
+                                  <v-checkbox
+                                    v-model="checkbox"
+                                    label="No"
+                                    hide-details
+                                    class="shrink mr-2 mt-0"
+                                  ></v-checkbox>
+                              </v-col>
+                            </v-row>
+                          </v-card-text>
+                          
+                          <v-col
+                  cols="12"
+                  md="4"
+                >
+                <p class="text-left">{{ radios || '¿Ha sido notificado por COFEPRIS?' }}</p>
+                
+                <v-radio-group
+                    v-model="row"
+                    row
+                  >
+                    <v-radio
+                      label="Si"
+                      value="radio-1"
+                    ></v-radio>
+                    <v-radio
+                      label="No"
+                      value="radio-2"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                <p class="text-left">{{ radios || '¿Cuenta con aviso de funcionamiento?' }}</p>
+                
+                <v-radio-group
+                    v-model="row"
+                    row
+                  >
+                    <v-radio
+                      label="Si"
+                      value="radio-1"
+                    ></v-radio>
+                    <v-radio
+                      label="No"
+                      value="radio-2"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                <p class="text-left">{{ radios || '¿Cuenta con cédula profesional?' }}</p>
+                
+                <v-radio-group
+                    v-model="row"
+                    row
+                  >
+                    <v-radio
+                      label="Si"
+                      @click="expand = !expand"
+                    ></v-radio>
+
+                    <v-radio
+                      label="No"
+                      @click="isdeleted"
+                    ></v-radio>
+
+                    <v-radio
+                      label="En trámite"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="5"
+                >
+                <p class="text-left">{{ radios || '¿Cuenta con redes sociales comerciales?' }}</p>
+                
+                <v-radio-group
+                    v-model="row"
+                    row
+                  >
+                    <v-radio
+                      label="Si"
+                      @click="expandi = !expandi"
+                    ></v-radio>
+
+                    <v-radio
+                      label="No"
+                    ></v-radio>
+
+                  </v-radio-group>
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                <p class="text-left">{{ radios || '¿Cuenta con sitio web propio?' }}</p>
+                <v-radio-group
+                    v-model="row"
+                    row
+                  >
+                    <v-radio
+                      label="Si"
+                      @click="expande = !expande"
+                    ></v-radio>
+
+                    <v-radio
+                      label="No"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-col>
+                <v-col 
+                    cols="12"
+                    md="3"
+                  ></v-col>
+                <v-col 
+                    cols="12"
+                    md="2"
+                  >
+                    <v-checkbox
+                    v-show="expandi"
+                        v-model="checkbox"
+                        hide-details
+                        label="Facebook "
+                        class="shrink mr-2 mt-0"
+                      ></v-checkbox>
+                      
+                  </v-col>
+                  <v-col 
+                    cols="12"
+                    md="3"
+                  >
+                    <v-checkbox
+                    v-show="expandi"
+                        v-model="checkbox"
+                        hide-details
+                        label="Instagram "
+                        class="shrink mr-2 mt-0"
+                      ></v-checkbox>
+                      
+                  </v-col>
+                
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                <v-text-field
+                v-show="expande"
+                    label="Sitio Web"
+                    outlined
+                    dense
+                    filled
+                  ></v-text-field></v-col>
+                  
+          </v-row>
           <v-btn
             color="primary"
-            @click="e1 = 1"
+            @click="e1 = 4"
           >
             Continue
           </v-btn>
@@ -595,6 +868,46 @@
             Atras
           </v-btn>
         </v-stepper-content>
+        <v-stepper-content step="4">
+
+          <p class="text-left">Observaciones</p>
+            <v-textarea
+              v-model="title"
+              label="Aquí puede escribir cualquier observación sobre los datos que ha introducido."
+              full-width
+              filled
+            ></v-textarea>
+
+            <v-col 
+              cols="12"
+              md="12"
+            >
+            <v-form
+            ref="form"
+      v-model="valid"
+      lazy-validation>
+
+      <v-switch
+      v-model="name"
+        :rules="nameRules"
+        required
+             
+                      :label="`Estoy de acuerdo con que esta información será tratada con plena confidencialidad por parte de Guía Salud.`"
+                ></v-switch>
+            </v-form>
+                
+            </v-col>
+<v-btn
+  color="primary"
+>
+  Finalizar
+</v-btn>
+
+<v-btn
+  @click="e1 = 1">
+  Atras
+</v-btn>
+</v-stepper-content>
       </v-stepper-items>
     </v-stepper>
                 </v-col>
@@ -645,19 +958,24 @@
 
 
 <script>
+  
   export default {
     data () {
+      
       return {
         tabs: null,
         e1: 1, 
         expand: false,
+        expandi: false,
+        expande: false,
         includeFiles: true,
-    enabled: false,
+        enabled: false,
         vue: {
           exp: null,
-         
         }
       }
     },
+    
   }
+  
 </script>
